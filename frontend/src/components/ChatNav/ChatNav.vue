927896f7c601761d8c3d6d710da9dbd18cc905ac
hututuyyds <129722237+hututuyyds@users.noreply.github.com>
2023-05-29 18:41:10 +0800

    Update ChatNav.vue

--- frontend/src/components/ChatNav/ChatNav.vue
+++ frontend/src/components/ChatNav/ChatNav.vue
@@ -30,8 +30,8 @@ const navType = {
 const navConfigs = [
   {
     key: navType.github,
-    label: '👍赞助支持',
-    url: 'https://github-production-user-asset-6210df.s3.amazonaws.com/120257354/238987102-8dd1c2f3-9b0b-44c8-a662-fd619043c0e9.jpg '
+    label: '赞助支持',
+    url: 'https://github-production-user-asset-6210df.s3.amazonaws.com/120257354/238987102-8dd1c2f3-9b0b-44c8-a662-fd619043c0e9.jpg ',
   },
   {
     key: navType.version,
